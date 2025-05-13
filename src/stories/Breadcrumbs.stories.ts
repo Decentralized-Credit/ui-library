9ee23f10d690185cbfa88d7d9d155836b1c32674
Josh Koter <josh.koter@gmail.com>
2025-05-13 23:05:04 +0200

    ...

---
+++ src/stories/Breadcrumbs.stories.ts
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Breadcrumbs from "@/components/custom/Breadcrumbs";
+
+
+
+
+const meta = {
+  title: "Components/Breadcrumbs",
+  component: Breadcrumbs,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+
+    className: {
+      control: "text",
+      description: "Additional CSS classes to add to the component",
+    },
+
+  },
+  args: {  },
+} satisfies Meta<typeof Breadcrumbs>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    breadcrumbs: [
+      { label: "Home", href: "/" },
+      { label: "About", href: "/about" },
+      { label: "Contact", href: "/contact" },
+    ],
+  },
+};
