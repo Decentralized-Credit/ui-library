da5f795da24936ef4bf19d9ed2a51da9b619c341
Josh Koter <josh.koter@gmail.com>
2025-05-19 15:35:29 +0200

    Update Storybook dependencies to version 8.6.14 for manager-api and theming in package.json and pnpm-lock.yaml.

---
+++ .storybook/manager.ts
@@ -0,0 +1,16 @@
+import { addons } from "@storybook/manager-api";
+import { create } from "@storybook/theming/create";
+
+const theme = create({
+  base: "dark",
+  fontBase: '"Space Grotesk", sans-serif',
+  fontCode: "monospace",
+
+  brandTitle: "Decentralized Credit UI Library",
+  brandUrl: "https://metaversal.gg",
+  brandTarget: "_self",
+});
+
+addons.setConfig({
+  theme: theme,
+});
